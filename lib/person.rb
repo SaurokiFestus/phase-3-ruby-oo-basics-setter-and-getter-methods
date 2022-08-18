3cd2765a320006a262a4bc0d6837bd26da59506a
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-08-18 10:50:13 +0300

    commit tree 5cc27272f871f1e2ac5ef3d023162facb2c167a2

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
-class Person
 
+class Person
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    #job setter method
+    def job=(job)
+        @job = job
+    end
+
+    #job getter method
+    def job
+        @job
+    end
 end
+
+kanye = Person.new
+
